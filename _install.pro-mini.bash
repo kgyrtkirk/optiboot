40cbb9f82b66655e7efded3c8ffe6c4b82726ffb
Haindrich Zoltán (kirk) <kirk@rxd.hu>
2016-06-30 22:57:19 +0200

    add preshared.key upload

--- _install.pro-mini.bash
+++ _install.pro-mini.bash
@@ -1,2 +1,15 @@
+#!/bin/bash
+set -e
+
+DEV=/dev/ftdi
+
 D=./optiboot/bootloaders/optiboot/
-make -C $D ISPTOOL=avrisp ISPPORT=/dev/ftdi ISPSPEED=-b19200  BAUD_RATE=57600 atmega328_isp LED_START_FLASHES=0
+make -C $D ISPTOOL=avrisp ISPPORT=$DEV ISPSPEED=-b19200  BAUD_RATE=57600 atmega328_isp LED_START_FLASHES=0
+if [ -e preshared.key ] ; then
+	sleep 3
+	avrdude  -c avrisp -p atmega328p -P $DEV -b 19200 \
+		-U eeprom:w:preshared.key:r
+else
+	banner skipping key
+fi
+banner ok
